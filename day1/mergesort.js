7bed2b897ed86a166c54e7019d9c5c819c936dec
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-21 15:38:07 +0530

    merge sort implementation 1

---
+++ day1/mergesort.js
@@ -0,0 +1,52 @@
+
+function merge(a,mid,b,larr,rarr)
+{
+    // console.log("left and right array values",larr,rarr)
+    // console.log("bound values",a,mid,b)
+    let ans  = []
+    let i = 0;
+    let j =  0
+    while( i<=mid-a && j<=b-mid-1)
+    {
+        if(larr[i]<rarr[j])
+        {
+            ans.push(larr[i])
+            i++
+        }
+        else{
+            ans.push(rarr[j])
+            j++
+        }
+    }
+    while(i<=mid-a)
+    {
+        ans.push(larr[i])
+        i++
+    }
+    while(j<=b-mid-1)
+    {
+        ans.push(rarr[j])
+        j++
+    }
+    // console.log("merged array:",ans)
+    // for(let k=a;k<=b;k++)
+    // {
+    //     arr[i] = ans[i-a]
+    // }
+    return ans;
+}
+function divide(a,b,arr)
+{
+    if(a===b)
+        return [arr[a]]
+    let mid =   Math.floor((a+b)/2)
+    // console.log("mid value",mid)
+    let larr = divide(a,mid,arr)
+    let rarr = divide(mid+1,b,arr)
+    return merge(a,mid,b,larr,rarr)
+}
+
+const arr = [1,10,3,5,2,7]
+let a = 0 ,b = arr.length-1
+console.log(divide(a,b,arr))
+// console.log("final answer",arr)
