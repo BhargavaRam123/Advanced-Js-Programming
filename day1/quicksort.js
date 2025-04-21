2b87f5dc93505721efd60974ac3c1c881613cb06
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-21 17:35:09 +0530

    added importance of quicksort

--- day1/quicksort.js
+++ day1/quicksort.js
@@ -15,3 +15,10 @@ function quicksortfunc(arr) {
 
 let arr = [3, 1, 6, 2];
 console.log(quicksortfunc(arr));
+
+// It is a divide-and-conquer algorithm that makes it easier to solve problems.
+// It is efficient on large data sets.
+// It has a low overhead, as it only requires a small amount of memory to function.
+// It is Cache Friendly as we work on the same array to sort and do not copy data to any auxiliary array.
+// Fastest general purpose algorithm for large data when stability is not required.
+// It is tail recursive and hence all the tail call optimization can be done.
