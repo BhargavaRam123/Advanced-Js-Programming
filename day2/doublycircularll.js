06412ff091ad111610e6ef94cda2cb639f34fb2d
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-22 16:28:13 +0530

    Insert a node at the beginning of the list

--- day2/doublycircularll.js
+++ day2/doublycircularll.js
@@ -5,3 +5,33 @@ class Node {
     this.prev = null;
   }
 }
+
+class DoublyCircularLinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  insertAtBeginning(data) {
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+      newNode.next = newNode;
+      newNode.prev = newNode;
+    } else {
+      const lastNode = this.head.prev;
+
+      newNode.next = this.head;
+      newNode.prev = lastNode;
+
+      this.head.prev = newNode;
+      lastNode.next = newNode;
+
+      this.head = newNode;
+    }
+
+    this.size++;
+    return this;
+  }
+}
