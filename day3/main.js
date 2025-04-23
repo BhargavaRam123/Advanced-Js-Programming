51b2b916448cf11d67dd191ae7a63ace7ba404fc
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-23 12:23:45 +0530

    arrow vs normal function

--- day3/main.js
+++ day3/main.js
@@ -5,3 +5,35 @@ let a = 1,
   b = 2,
   c = 3;
 func(a, b, c);
+
+const newarr = [1, 3, 4, 5, 5];
+function func1(a, b, c, d, e) {
+  console.log(a, b, c, e);
+}
+func1(...newarr);
+
+const person = {
+  name: "Alice",
+  sayHiRegular: function () {
+    console.log(`Hi, I'm ${this.name}`);
+  },
+  sayHiArrow: () => {
+    console.log(`Hi, I'm ${this.name}`);
+  },
+
+  friendsGreeting: function () {
+    console.log(`${this.name}'s friends say:`);
+
+    setTimeout(function () {
+      console.log(`Regular function: Hi, ${this.name}!`); // 'this' is window/global object
+    }, 100);
+
+    setTimeout(() => {
+      console.log(`Arrow function: Hi, ${this.name}!`); // 'this' is person object
+    }, 100);
+  },
+};
+
+person.sayHiRegular();
+person.sayHiArrow();
+person.friendsGreeting();
