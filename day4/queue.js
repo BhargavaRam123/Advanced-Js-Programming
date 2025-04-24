51d3e09f7a2c529b28c965c12cc6d4eafc78f6ad
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-24 11:43:38 +0530

    Add Queue class implementation with example usage; remove default.js

---
+++ day4/queue.js
@@ -0,0 +1,15 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+}
+
+// Example usage:
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.print();
+console.log(queue.dequeue());
+console.log(queue.peek());
+console.log(queue.size());
