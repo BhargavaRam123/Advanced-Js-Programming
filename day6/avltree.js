54307561a76cffc5beb9807c79e7bf5fdfaabd3d
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-26 11:44:55 +0530

    Implement insert method for AvlTree class with helper function for node insertion and balancing

--- day6/avltree.js
+++ day6/avltree.js
@@ -11,5 +11,24 @@ class AvlTree {
   constructor() {
     this.root = null;
   }
-  insert(value) {}
+  insert(value) {
+    const inserthelper = (node, parent = null) => {
+      if (!node) {
+        const newNode = new AvlTreeNode(value);
+        newNode.parent = parent;
+        return newNode;
+      }
+      if (value < node.value) {
+        node.left = inserthelper(node.left, node);
+      } else if (value > node.value) {
+        node.right = inserthelper(node.right, node);
+      }
+      this._updateHeight(node);
+      return this._balance(node);
+    };
+    this.root = inserthelper(this.root);
+    if (this.root.parent) {
+      this.root.parent = null;
+    }
+  }
 }
