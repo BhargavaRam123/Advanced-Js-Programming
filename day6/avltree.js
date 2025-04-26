6001b07879938a0c04c5460a9e9d4d6178704c3b
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-26 16:59:39 +0530

    Fix _rightrotation method signature and update insertion examples in AvlTree class

--- day6/avltree.js
+++ day6/avltree.js
@@ -71,7 +71,7 @@ class AvlTree {
     this._updateHeight(temp);
     return temp;
   }
-  _rightrotation() {
+  _rightrotation(node) {
     const temp = node.left;
     node.left = temp.right;
     if (temp.right) {
@@ -158,7 +158,17 @@ class AvlTree {
 }
 
 const tr = new AvlTree();
-tr.insert(1);
+tr.insert(21);
+tr.insert(26);
+tr.insert(30);
+tr.insert(9);
+tr.insert(4);
+tr.insert(14);
+tr.insert(28);
+tr.insert(18);
+tr.insert(15);
+tr.insert(10);
 tr.insert(2);
 tr.insert(3);
+tr.insert(7);
 console.log(tr.levelOrderTraversal());
