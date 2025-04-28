48afa27bcd51bf7c3f93e794a4a9f1b86dfb3708
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-28 14:58:28 +0530

    Implement BFS algorithm within Graph class constructor

--- day7/bfs.js
+++ day7/bfs.js
@@ -6,6 +6,27 @@ class Graph {
     {
       this.adj[v].push(w);
     }
+    bfs();
+    {
+      let visited = new Array(this.V).fill(false);
+      for (let i = 0; i < this.V; i++) {
+        if (!visited[i]) {
+          let queue = [];
+          visited[i] = true;
+          queue.push(i);
+          while (queue.length !== 0) {
+            let s = queue.shift();
+            console.log("visited", s);
+            for (let n of this.adf[s]) {
+              if (!visited[n]) {
+                visited[n] = true;
+                queue.push(n);
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 
