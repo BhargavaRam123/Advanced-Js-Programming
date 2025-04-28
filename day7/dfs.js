15c77f34dd33e5aa53a20168c7436794d549a8ea
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-28 15:20:55 +0530

    Implement depth-first search (DFS) method in graph class

--- day7/dfs.js
+++ day7/dfs.js
@@ -6,5 +6,22 @@ class graph {
   addEdge(v, w) {
     this.adj[v].push(w);
   }
-  dfs() {}
+  dfs() {
+    let visited = new Array(this.v).fill(false);
+    for (let i = 0; i < this.v; i++) {
+      visited[i] = true;
+      console.log("visited node is ", i);
+      let st = [];
+      while (st.length !== 0) {
+        for (let n of this.adj[i]) {
+          let e = st.pop();
+          console.log("visited node ", e);
+          if (!visited[n]) {
+            st.push(n);
+            visited[n] = true;
+          }
+        }
+      }
+    }
+  }
 }
