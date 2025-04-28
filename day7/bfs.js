068d1bd392a92ef7cb1a8cbed4a0f3f13de987ef
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-28 16:40:30 +0530

    Refactor BFS implementation: replace Graph class with bfs class and update traversal logic

--- day7/bfs.js
+++ day7/bfs.js
@@ -1,38 +1,64 @@
-class Graph {
-  constructor(v) {
-    this.V = v;
-    this.adj = new Array(v).fill(null).map(() => []);
+// class Graph {
+//   constructor(v) {
+//     this.V = v;
+//     this.adj = new Array(v).fill(null).map(() => []);
+//   }
+//   addEdge(v, w) {
+//     this.adj[v].push(w);
+//   }
+//   bfs() {
+//     let visited = new Array(this.V).fill(false);
+//     for (let i = 0; i < this.V; i++) {
+//       if (!visited[i]) {
+//         let queue = [];
+//         visited[i] = true;
+//         queue.push(i);
+//         while (queue.length !== 0) {
+//           let s = queue.shift();
+//           console.log("visited", s);
+//           for (let n of this.adj[s]) {
+//             if (!visited[n]) {
+//               visited[n] = true;
+//               queue.push(n);
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
+// let g = new Graph(4);
+// g.addEdge(0, 1);
+// g.addEdge(0, 2);
+// g.addEdge(1, 3);
+// g.addEdge(1, 2);
+// // g.addEdge(1, 3);
+// g.addEdge(2, 3);
+// g.bfs();
+
+class bfs {
+  constructor() {
+    this.adjacencylist = {};
   }
-  addEdge(v, w) {
-    this.adj[v].push(w);
+  addvertex(v) {
+    this.adjacencylist[v] = [];
   }
-  bfs() {
-    let visited = new Array(this.V).fill(false);
-    for (let i = 0; i < this.V; i++) {
-      if (!visited[i]) {
-        let queue = [];
-        visited[i] = true;
-        queue.push(i);
-        while (queue.length !== 0) {
-          let s = queue.shift();
-          console.log("visited", s);
-          for (let n of this.adj[s]) {
-            if (!visited[n]) {
-              visited[n] = true;
-              queue.push(n);
-            }
-          }
-        }
-      }
+  addedge(v, w) {
+    this.adjacencylist[v].push(w);
+    this.adjacencylist[w].push(v);
+  }
+  bfstraversal(start) {
+    let queue = [start];
+    let result = [];
+    let visited = {};
+    while (queue.length > 0) {
+      let value = queue.shift();
+      visited[value] = true;
+      result.push(value);
+      this.adjacencylist[value].forEach((element) => {
+        if (!visited[element]) value.push(element);
+      });
     }
   }
 }
-
-let g = new Graph(4);
-g.addEdge(0, 1);
-g.addEdge(0, 2);
-g.addEdge(1, 3);
-g.addEdge(1, 2);
-// g.addEdge(1, 3);
-g.addEdge(2, 3);
-g.bfs();
