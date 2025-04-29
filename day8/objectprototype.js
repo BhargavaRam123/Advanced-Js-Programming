97ebe2e7fd652c6524068c9e7b00155b7c42fd56
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-29 11:49:21 +0530

    add example demonstrating valueOf method with Number object

--- day8/objectprototype.js
+++ day8/objectprototype.js
@@ -14,3 +14,7 @@ let obj2 = {
   name: "value",
 };
 console.log(obj2.valueOf());
+
+const num = new Number("5");
+console.log(typeof num); // logs: "object"
+console.log(num ** 2); // logs: 25 // hint is "number", so method valueOf() returns the primitive value; the number 25 in this case.
