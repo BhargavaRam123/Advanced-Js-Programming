cd6c3ba02334ba7133e4a007887a3483839b6c5f
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-29 10:58:35 +0530

    add number object logging to main.js

--- day8/main.js
+++ day8/main.js
@@ -2,3 +2,9 @@ const obj = { name: "hello", age: 10 };
 console.log(obj);
 console.log(obj.toString());
 console.log(obj.valueOf());
+
+const num = new Number(1);
+console.log(num);
+console.log(typeof num);
+console.log(num.valueOf());
+console.log(num.toString());
