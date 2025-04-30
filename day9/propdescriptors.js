08653e55c5c7835b6af0210da3f4bedd6bc0bfd5
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-30 11:04:33 +0530

    Enhance property descriptor example by adding age property and logging object keys

--- day9/propdescriptors.js
+++ day9/propdescriptors.js
@@ -1,10 +1,15 @@
 let obj = {
   name: "hello",
+  age: 10,
 };
 const des = Object.getOwnPropertyDescriptor(obj, "name");
 console.log(des);
 Object.defineProperty(obj, "name", {
   writable: false,
+  enumerable: false,
 });
 obj.name = "change";
+for (let i in obj) {
+  console.log("obj keys are:", i);
+}
 console.log(obj);
