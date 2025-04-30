15b9abecb1b4f3fbaa03130d59b3f504449b741c
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-30 11:13:54 +0530

    you can only change wirtable from true to false while it is not configurable not the vice versa

--- day9/propdescriptors.js
+++ day9/propdescriptors.js
@@ -5,11 +5,15 @@ let obj = {
 const des = Object.getOwnPropertyDescriptor(obj, "name");
 console.log(des);
 Object.defineProperty(obj, "name", {
-  writable: false,
+  writable: true,
   enumerable: false,
+  configurable: false,
 });
 obj.name = "change";
 for (let i in obj) {
   console.log("obj keys are:", i);
 }
-console.log(obj);
+console.log("object name changed or not :", obj.name);
+Object.defineProperty(obj, "name", {
+  writable: false,
+});
