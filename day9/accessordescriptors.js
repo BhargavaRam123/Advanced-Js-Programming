4a507efc20fcb7f837b9423d0419ac978779d5a7
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-30 12:50:08 +0530

    Add user object with getter and setter for name validation

--- day9/accessordescriptors.js
+++ day9/accessordescriptors.js
@@ -16,3 +16,22 @@ obj.fullname = "kimbel musk";
 console.log(obj.fullname);
 console.log("All accessor descriptors:", desc);
 console.log("entire obj is ", obj);
+
+let user = {
+  get name() {
+    return this._name;
+  },
+
+  set name(value) {
+    if (value.length < 4) {
+      alert("Name is too short, need at least 4 characters");
+      return;
+    }
+    this._name = value;
+  },
+};
+
+user.name = "Pete";
+alert(user.name); // Pete
+
+user.name = ""; // Name is too short...
