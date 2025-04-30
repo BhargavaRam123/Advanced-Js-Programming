464afcfeb8c2096639ea87d17dcdfab3d26710c6
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-30 12:04:12 +0530

    Implement setter for fullname to update first and last names

--- day9/gettersetter.js
+++ day9/gettersetter.js
@@ -4,5 +4,12 @@ const obj = {
   get fullname() {
     return this.fname + " " + this.lname;
   },
+  set fullname(value) {
+    [this.fname, this.lname] = value.split(" ");
+  },
 };
+console.log(obj.fullname);
+
+obj.fullname = "kimbel musk";
+
 console.log(obj.fullname);
